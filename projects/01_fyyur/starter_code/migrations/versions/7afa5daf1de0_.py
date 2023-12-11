eaf800a5df738bac75d4a4bd15141048ba2b127a
Michael Smeal <michaelasmeal@gmail.com>
2023-12-11 16:17:57 -0700

    migration goodies

---
+++ projects/01_fyyur/starter_code/migrations/versions/7afa5daf1de0_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 7afa5daf1de0
+Revises: 
+Create Date: 2023-09-18 18:33:21.838321
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '7afa5daf1de0'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
